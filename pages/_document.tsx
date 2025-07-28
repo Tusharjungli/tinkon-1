2e77e971ce813e52f44bf34e283a4e7597756ce1
Tusharjungli <trpanchaltr@gmail.com>
2025-07-28 18:08:17 +0530

    added code for ads in twitter

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+
         {/* Basic SEO */}
         <meta
           name="description"
@@ -53,6 +54,30 @@ export default function Document() {
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+
+        {/* Twitter Conversion Tracking Base Code */}
+        <Script
+          id="twitter-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(e,t,n,s,u,a){
+                e.twq||(s=e.twq=function(){
+                  s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+                },
+                s.version='1.1',
+                s.queue=[],
+                u=t.createElement(n),
+                u.async=!0,
+                u.src='https://static.ads-twitter.com/uwt.js',
+                a=t.getElementsByTagName(n)[0],
+                a.parentNode.insertBefore(u,a))
+              }(window,document,'script');
+              twq('config','q87ph');
+            `,
+          }}
+        />
+        {/* End Twitter conversion tracking base code */}
       </Head>
       <body>
         <Main />
